a9fbdebefbcd3b744b3fff585f145a5ebae20e0c
unknown <ae2004w@gmail.com>
2023-08-08 15:45:11 -0400

    updated order page as per naveens comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,5 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
+	
+	
+	By loc = By.id("order");
+	
+	public void getOrder() {
+		System.out.println("get order");
+	}
+	
+	
 
 }
