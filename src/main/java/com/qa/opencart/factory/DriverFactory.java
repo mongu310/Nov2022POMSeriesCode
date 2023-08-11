84538603aefe063a2f2978e773c244b2f619c3c5
unknown <ae2004w@gmail.com>
2023-08-11 12:15:33 -0400

    deleted files

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.opencart.exception.FrameworkException;
